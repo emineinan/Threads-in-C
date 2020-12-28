27f58b92cc0f143d152dd8de896cc77d8cfb1fce
emineinan <56589369+emineinan@users.noreply.github.com>
2020-12-28 17:53:08 +0300

    Add sample for mutex

---
+++ mutex.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int count=0;
+pthread_mutex_t mutex;
+
+void* routine()
+{
+    for(int i=0;i<10000000;i++){
+        pthread_mutex_lock(&mutex);
+        count++;
+        pthread_mutex_unlock(&mutex);
+    }
+}
+int main()
+{
+   pthread_t t1;
+   pthread_t t2;
+   pthread_mutex_init(&mutex,NULL);
+   
+   if(pthread_create(&t1,NULL,&routine,NULL)!=0){
+       return 1;
+   }
+   if(pthread_create(&t2,NULL,&routine,NULL)!=0){
+       return 2;
+   }
+   if(pthread_join(t1,NULL)!=0){
+       return 3;
+   }
+   if(pthread_join(t2,NULL)!=0){
+       return 4;
+   }
+   pthread_mutex_destroy(&mutex);
+   printf("count: %d\n",count);
+   return 0;
+}
